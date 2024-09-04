aa8ced37cf27a892c5a7e8c98e3979c211cd0378
KI JONG BYUN <kjbyun0@gmail.com>
2024-09-04 10:28:40 -0500

    Add my solution

--- javascript/tree_traversal_bfs.js
+++ javascript/tree_traversal_bfs.js
@@ -8,20 +8,71 @@ class Node {
 
 function levelOrderTraversal(root) {
   // type your code here
+  if (root === null)
+    return [];
+
+  const res = [];
+  const queue = [root, null];
+  let idx = 0;
+
+  while (idx < queue.length) {
+    const curNode = queue[idx++];
+    if (curNode !== null) {
+      if (curNode.left !== null)
+        queue.push(curNode.left);
+      if (curNode.right !== null)
+        queue.push(curNode.right);
+      res.push(curNode.value);
+    } else {
+      if (idx < queue.length)
+        queue.push(null);
+    }
+    // console.log("debug-curNode: ", curNode === null ? null : curNode.value);
+    // console.log("queue: ", queue, ", res: ", res);
+  }
+
+  return res;
 }
 
 if (require.main === module) {
   let root = new Node(1, new Node(2), new Node(3));
   // add your own tests in here
-  console.log("Expecting: [[1], [2, 3]]");
+  console.log("Expecting: [1, 2, 3]");
   console.log(levelOrderTraversal(root));
 
   console.log("");
 
   root = new Node(10, new Node(20, new Node(9), new Node(22)), new Node(30));
 
-  console.log("Expecting: [[10], [20, 30], [9, 22]]");
+  console.log("Expecting: [10, 20, 30, 9, 22]");
   console.log(levelOrderTraversal(root));
+
+  /////////////////////////////////////////////////////
+  // My own test cases
+  root = new Node(1);
+  console.log("");
+  console.log("Expecting: [1]");
+  console.log(levelOrderTraversal(root));
+
+  root.left = new Node(2);
+  console.log("");
+  console.log("Expecting: [1, 2]");
+  console.log(levelOrderTraversal(root));
+
+  root.right = new Node(3);
+  console.log("");
+  console.log("Expecting: [1, 2, 3]");
+  console.log(levelOrderTraversal(root));
+
+  root = new Node(1);
+  root.left = new Node(2);
+  root.left.right = new Node(3);
+  console.log("");
+  console.log("Expecting: [1, 2, 3]");
+  console.log(levelOrderTraversal(root));
+
+  console.log("");
+  console.log(levelOrderTraversal(new Node(5)));
 }
 
 module.exports = {
